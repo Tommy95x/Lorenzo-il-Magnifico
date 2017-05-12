043e401be830e3a0f7c27d928fcb861950bb251b
Tommy <tommaso.grilli@mail.polimi.it>
2017-05-12 11:47:42 +0200

    Lorenzo il Magnifico v1.1

--- src/server/DataBaseConnection.java
+++ src/server/DataBaseConnection.java
@@ -1,5 +1,6 @@
 package server;
-/*Classe utilizzata per ogni istanzia di una connesisone da parte di tutte le classi che richiedono una acquisizione al DB per
+/*
+*Classe utilizzata per ogni istanzia di una connesisone da parte di tutte le classi che richiedono una acquisizione al DB per
 *richiedere informazioni
 **/
 public Class DataBaseConnection{
