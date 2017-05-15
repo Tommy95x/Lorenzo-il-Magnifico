a3d62903f744703b3b1a7b3cc0daa48ada882a3d
Tommy <tommaso.grilli@mail.polimi.it>
2017-05-15 13:59:59 +0200

    Update ImplementsServerInterface.java

--- src/server/ImplementsServerInterface.java
+++ src/server/ImplementsServerInterface.java
@@ -1,5 +1,9 @@
 package server;
 
+/*
+*Classe per l'implementazione dei metodi utilizzati nelle classi RMIServer e ThreadSocketServer, meodoti richiamati e utilizzati per giocare
+*dai giocatori durante una partita.
+**/
 public class ImplementsRMIServerInterface implements RMIServerInterface{
 
 }
