9ea3f30ba64f6b2ba621ff3dd7c55beb8d35c9bd
tommy <tommy@tommy-HP-PC>
2017-05-16 11:12:05 +0200

    Sistemato connessione DB

--- src/server/database/PoolDatabase.java
+++ src/server/database/PoolDatabase.java
@@ -1,7 +1,22 @@
 package server.database;
 
+import java.sql.Connection;
+
 public class PoolDatabase {
 
-	
+	//Array di connessioni al database
+		Connection con[];
+		
+		//Array disponibilità connessioni
+		boolean busy[];
+		
+		//Registro chi tiene occupata la connessione
+		String name[];
+		
+		//Numero connessioni attive
+		int numconn;
+		
+		//incremento dimensione pool per nuove richieste
+		int inc;
 	
 }
