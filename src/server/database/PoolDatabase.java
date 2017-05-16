2d657c2ec28773421e00600d059f09fd0ab629ae
tommy <tommy@tommy-HP-PC>
2017-05-16 11:14:59 +0200

    Pool DB

--- src/server/database/PoolDatabase.java
+++ src/server/database/PoolDatabase.java
@@ -5,18 +5,18 @@ import java.sql.Connection;
 public class PoolDatabase {
 
 	//Array di connessioni al database
-		Connection con[];
+	private Connection con[];
 		
-		//Array disponibilità connessioni
-		boolean busy[];
+	//Array disponibilità connessioni
+	private	boolean busy[];
 		
-		//Registro chi tiene occupata la connessione
-		String name[];
+	//Registro chi tiene occupata la connessione
+	private	String name[];
 		
-		//Numero connessioni attive
-		int numconn;
+	//Numero connessioni attive
+	private	int numconn;
 		
-		//incremento dimensione pool per nuove richieste
-		int inc;
+	//incremento dimensione pool per nuove richieste
+	private	int inc;
 	
 }
