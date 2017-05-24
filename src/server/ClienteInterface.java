144817882acc54fb197f983d896a73a42a57acbb
Tommy <tommaso.grilli@mail.polimi.it>
2017-05-24 12:30:06 +0200

    Create ClienteInterface.java

---
+++ src/server/ClienteInterface.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+import server.element.Partita;
+
+public interface ClientInterface extends Remote{
+
+	public String login(String account, String pw);
+	public String register(String account, String pw, String pw2, String email);
+	public boolean createANewLobby(String lobby);
+	public ArrayList<Partita> lobbiesView();
+	public void enterInALobby(String lobby);
+	public void selectColorGamer(String color);
+	public void startGame();
+	public void lanciaDadi();
+	public void posizionareFamiliare(String color, int x, int y);
+	public void spendereRisorse(String risorsa, int qta);
+	public void takeCards(String name);
+	public void sostegnoChiesa(boolean flag);
+	public void richiestaRegistrazione();
+	
+}
